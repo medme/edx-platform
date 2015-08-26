b8651831483d622856d010c2317a829929c6abed
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2015-08-26 18:25:47 +0500

    Added shim for moment in main_requirejs

--- common/lib/xmodule/xmodule/js/spec/main_requirejs.js
+++ common/lib/xmodule/xmodule/js/spec/main_requirejs.js
@@ -2,6 +2,9 @@
     requirejs.config({
         paths: {
             "moment": "xmodule/include/common_static/js/vendor/moment.min"
+        },
+        "moment": {
+            exports: "moment"
         }
     });
 
